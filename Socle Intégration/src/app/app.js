d95d45122b2c46d68ec98c3d01bc3114801902a8
Claire <cc.cariou.claire@gmail.com>
2017-03-07 20:21:24 +0100

    Initialisation Socle Intégration

---
+++ Socle Intégration/src/app/app.js
@@ -0,0 +1,11 @@
+/* CSS LIB */
+require('material-design-lite/material.css');
+
+/* JS LIB */
+global.$ = global.jQuery = require('jquery');
+
+/* CSS */
+require('./css/app.less');
+
+/* JS Project */
+require('./js/index');
